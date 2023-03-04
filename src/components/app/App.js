8e17cb754097b22cca08c08e197dd651503dad91
Dmitry Makarov <makdmt@gmail.com>
2023-03-04 08:49:04 +0300

    feat: дабавить подключение к api

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -9,13 +9,59 @@ import { ingredients } from '../../utils/data';
 import appStyles from './app.module.css';
 
 function App(): JSX.Element {
+
+  const [state, setState] = React.useState({
+    isLoading: false,
+    hasError: false,
+    ingredients: []
+  })
+
+  React.useEffect(() => {
+    const getIngredients = async (url) => {
+      try {
+        setState({ ...state, isLoading: true });
+        const res = await fetch(url);
+        const data = await res.json();
+        setState({ ...state, isLoading: false, ingredients: data.data});
+        console.log(data);
+        console.log(state);
+      } catch (err) {
+        setState({ ...state, isLoading: false, hasError: true })
+      }
+    }
+
+    // React.useEffect(() => {
+    //   const getIngredients = async(url) => {
+    //     setState({...state, isLoading: true});
+    //     fetch(url)
+    //     .then(res => res.json())
+    //     .then(data => setState({...state,isLoading: false, ingredients: data.data}))
+    //     .catch(err => setState({...state, isLoading: false, hasError: true}))
+    //   }
+
+    getIngredients('https://norma.nomoreparties.space/api/ingredients');
+
+  },[])
+  // console.log(state);
+
+
   return (
     <div className={appStyles.page}>
-      <AppHeader/>
+      <AppHeader />
+
       <main className={appStyles.main}>
-        <BurgerIngredients products={ingredients} />
-        <BurgerConstructor mainIngredients={ingredients} />
+        {state.isLoading && 'Загрузка...'}
+        {state.hasError && 'Произошла ошибка, попробуйте перезагрузить страницу'}
+
+        {!state.isLoading && !state.hasError && state.ingredients.length &&
+          <>
+            <BurgerIngredients products={state.ingredients} />
+            <BurgerConstructor mainIngredients={state.ingredients} />
+          </>
+
+        }
       </main>
+
     </div>
   );
 }
